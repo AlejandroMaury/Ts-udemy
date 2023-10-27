e06dccc7fab508e980de5f5c0bd85a634bdcef56
Alejandro Arango <alejandroarang@gmail.com>
2023-10-26 20:25:17 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+    return a + b;
+}
+var a = 5;
+var b = 6;
+var result = add(a, b);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,9 @@
+function add(a:number,b:number){
+    return a+b;
+}
+
+const a= 5;
+const b=6;
+
+const result = add(a,b);
+console.log(result)
